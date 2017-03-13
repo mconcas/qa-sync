a97e9bf963d23c0f313bb84515a4412cdfc34bf4
Matteo Concas <mconcas@cern.ch>
2017-03-13 18:35:05 +0100

    Update qa-AFS-inodes-count.sh

--- qa-AFS-inodes-count.sh
+++ qa-AFS-inodes-count.sh
@@ -6,12 +6,12 @@ mailto=$2
 
 # Check detector name and path
 : ${detector:?"Detector must specified!"}
-dest_path="/afs/cern.ch/work/a/aliqa$detector/www"
-registry_file="/afs/cern.ch/user/a/aliqamod/inodes-$detector.txt"
-stat $dest_path
+afs_path="/afs/cern.ch/work/a/aliqa$detector/www"
+registry_file="/afs/cern.ch/user/a/aliqamod/workbench/registries/inodes-$detector.txt"
+stat $afs_path > /dev/null
 
 ### Main
 date=$(date +%s)
 inodes=$(find "$afs_path"/ | wc -l)
 echo -e "$date\t$inodes" >> $registry_file
-echo $inodes | nail -s "Inode count for $detector" $mailto
+echo $inodes | nail -s "Inode count for $detector" $mailto 2>&1 > /dev/null
